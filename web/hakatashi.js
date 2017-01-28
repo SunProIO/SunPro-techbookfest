2c0df284371fef79d7a243bd6fb965894b189eba
Koki Takahashi <hakatasiloving@gmail.com>
2017-01-29 02:16:04 +0900

    Fix indent

--- web/hakatashi.js
+++ web/hakatashi.js
@@ -1,30 +1,30 @@
-  {
-    config: {
-      title: 'SunPro 技術書典',
-      author: 'SunPro',
-      email: 'admin@sunpro.io',
+{
+  config: {
+    title: 'SunPro 技術書典',
+    author: 'SunPro',
+    email: 'admin@sunpro.io',
+  },
+  page: {
+    title: 'もし<wbr>インターネットの<wbr>1秒が<wbr>1年だったら',
+    subtitle: 'hakatashi',
+    content: fs.readFileSync('../hakatashi.html'),
+  },
+  theme: {
+    'navbar-links': [],
+    footer: [],
+    avatar: '/../../favicon.png',
+    comment: {
+      enable: false,
     },
-    page: {
-      title: 'もし<wbr>インターネットの<wbr>1秒が<wbr>1年だったら',
-      subtitle: 'hakatashi',
-      content: fs.readFileSync('../hakatashi.html'),
+    highlight: {
+      enable: true,
     },
-    theme: {
-      'navbar-links': [],
-      footer: [],
-      avatar: '/../../favicon.png',
-      comment: {
-        enable: false,
-      },
-      highlight: {
-        enable: true,
-      },
-    },
-    url_for: (path) => {
-      if (path === undefined) {
-        return './';
-      } else {
-        return `beautiful-hexo/source${path}`;
-      }
-    },
-  }
+  },
+  url_for: (path) => {
+    if (path === undefined) {
+      return './';
+    } else {
+      return `beautiful-hexo/source${path}`;
+    }
+  },
+}
